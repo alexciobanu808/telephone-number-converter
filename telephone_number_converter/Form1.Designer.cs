cf438ec219f279fc30e62e5110a41295d47cd35e
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2021-08-25 19:18:18 -0400

    initial commit

---
+++ telephone_number_converter/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace TelephoneNumberConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.alphaNumTextBox = new System.Windows.Forms.TextBox();
+            this.convertionLabel = new System.Windows.Forms.Label();
+            this.convNumLabel = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(31, 22);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(331, 32);
+            this.instructionLabel.TabIndex = 0;
+            this.instructionLabel.Text = "Enter a telephone number containing letters in the \r\nfollowing format: XXX-XXXXXX" +
+    "XX (e.g. 800-FLOWERS). \r\n";
+            this.instructionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // alphaNumTextBox
+            // 
+            this.alphaNumTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.alphaNumTextBox.Location = new System.Drawing.Point(123, 77);
+            this.alphaNumTextBox.Name = "alphaNumTextBox";
+            this.alphaNumTextBox.Size = new System.Drawing.Size(142, 22);
+            this.alphaNumTextBox.TabIndex = 1;
+            this.alphaNumTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // convertionLabel
+            // 
+            this.convertionLabel.AutoSize = true;
+            this.convertionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertionLabel.Location = new System.Drawing.Point(70, 125);
+            this.convertionLabel.Name = "convertionLabel";
+            this.convertionLabel.Size = new System.Drawing.Size(249, 16);
+            this.convertionLabel.TabIndex = 2;
+            this.convertionLabel.Text = "This is your number\'s numeric equvalent.";
+            // 
+            // convNumLabel
+            // 
+            this.convNumLabel.BackColor = System.Drawing.SystemColors.Window;
+            this.convNumLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.convNumLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convNumLabel.Location = new System.Drawing.Point(123, 171);
+            this.convNumLabel.Name = "convNumLabel";
+            this.convNumLabel.Size = new System.Drawing.Size(142, 23);
+            this.convNumLabel.TabIndex = 3;
+            this.convNumLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(61, 228);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(115, 33);
+            this.convertButton.TabIndex = 4;
+            this.convertButton.Text = "Convert Number";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(204, 228);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(115, 33);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 301);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.convNumLabel);
+            this.Controls.Add(this.convertionLabel);
+            this.Controls.Add(this.alphaNumTextBox);
+            this.Controls.Add(this.instructionLabel);
+            this.Name = "Form1";
+            this.Text = "Telephone Number Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.TextBox alphaNumTextBox;
+        private System.Windows.Forms.Label convertionLabel;
+        private System.Windows.Forms.Label convNumLabel;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ telephone_number_converter/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelephoneNumberConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsValidFormat(string str)
+        {
+            //Declare variables
+            const int VALID_LENGTH = 11;
+            bool isValid;
+
+            //Check for length and '-' char
+            if (str.Length == VALID_LENGTH && str[3] == '-')
+            {
+                isValid = true;
+            }
+            else
+            {
+                isValid = false;
+            }
+
+            //Remove '-' char and check if remaining chars are alphanumeric
+            str = str.Remove(3, 1);
+            
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(str[i]))
+                    isValid = false;
+            }
+
+            return isValid;
+        }
+
+        private void ConvertToNumeric(ref string str)
+        {
+            //Convert any lowercase letters to uppercase
+            str = str.ToUpper();
+            
+            //Convert any alpha char to its int value
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'A' || str[i] == 'B' || str[i] == 'C')
+                {
+                    str = str.Remove(i,1);
+                    str = str.Insert(i, "2");
+                }
+                else if (str[i] == 'D' || str[i] == 'E' || str[i] == 'F')
+                {
+                    str = str.Remove(i, 1);
+                    str = str.Insert(i, "3");
+                }
+                else if (str[i] == 'G' || str[i] == 'H' || str[i] == 'I')
+                {
+                    str = str.Remove(i, 1);
+                    str = str.Insert(i, "4");
+                }
+                else if (str[i] == 'J' || str[i] == 'K' || str[i] == 'L')
+                {
+                    str = str.Remove(i, 1);
+                    str = str.Insert(i, "5");
+                }
+                else if (str[i] == 'M' || str[i] == 'N' || str[i] == 'O')
+                {
+                    str = str.Remove(i, 1);
+                    str = str.Insert(i, "6");
+                }
+                else if (str[i] == 'P' || str[i] == 'Q' || str[i] == 'R' || str[i] == 'S')
+                {
+                    str = str.Remove(i, 1);
+                    str = str.Insert(i, "7");
+                }
+                else if (str[i] == 'T' || str[i] == 'U' || str[i] == 'V')
+                {
+                    str = str.Remove(i, 1);
+                    str = str.Insert(i, "8");
+                }
+                else if (str[i] == 'W' || str[i] == 'X' || str[i] == 'Y' || str[i] == 'Z')
+                {
+                    str = str.Remove(i, 1);
+                    str = str.Insert(i, "9");
+                }
+            }
+        }
+        
+        private void convertButton_Click(object sender, EventArgs e)
+        {            
+            //Get and trim user input
+            string input = alphaNumTextBox.Text.Trim();
+
+            //Validate, convert, and display user input
+            if (IsValidFormat(input))
+            {
+                ConvertToNumeric(ref input);
+                convNumLabel.Text = input;
+            }
+            else
+            {
+                //Display error message if invalid entry
+                MessageBox.Show("Invalid Entry: The number you enter may only contain numbers, letters, " +
+                    "and one hyphen in this format: XXX-XXXXXXXX (e.g. 800-FLOWERS)");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //Exit the application
+            this.Close();
+        }
+    }
+}
